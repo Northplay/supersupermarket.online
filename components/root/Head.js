2b677a474614003e871b20fe92c5e4417d59c2d6
Kristian Andersen <hello@kristian.co>
2018-11-15 14:15:53 +0100

    Removed nprogress. Added shareimage

--- components/root/Head.js
+++ components/root/Head.js
@@ -4,14 +4,14 @@ import Head from 'next/head';
 const Meta = () => <Head>
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <meta charSet="utf-8" />
-  <link rel="stylesheet" type="text/css" href="/static/nprogress.css"/>
   <title>Super Supermarket ~ Race through supermarkets!</title>
   <meta name="description" content="Race through supermarkets! Collect as many groceries as you can! Upgrade your cart and win a spot on the local or global shopping list!"/>
   <meta name="twitter:card" value="summary" />
   <meta property="og:title" content="Super Supermarket" />
   <meta property="og:type" content="website" />
-  <meta property="og:url" content="http://supersupermarket.online/" />
-  <meta property="og:image" content="http://supersupermarket.online/image.jpg" />
+  <meta property="fb:app_id" content="613798989021124" />
+  <meta property="og:url" content="http://supersupermarket.online" />
+  <meta property="og:image" content="http://supersupermarket.online/static/shareimage.png" />
   <meta property="og:description" content="Race through supermarkets! Collect as many groceries as you can! Upgrade your cart and win a spot on the local or global shopping list!" />
 </Head>;
 
